f022fc6b7d6ab6551505dae0ba798ff9c8b2c423
Chaithra <ckulal@redhat.com>
2025-03-27 11:41:32 +0530

    d3n cache baremetal
    Signed-off-by: Chaithra <ckulal@redhat.com>

--- rgw/v2/tests/s3cmd/test_d3n_cache_s3cmd.py
+++ rgw/v2/tests/s3cmd/test_d3n_cache_s3cmd.py
@@ -1,32 +1,57 @@
-import os
-import sys
-
-sys.path.append(os.path.abspath(os.path.join(__file__, "../../../..")))
 import argparse
+import datetime
 import json
 import logging
+import os
+import socket
+import sys
 import time
 import traceback
 
-import v2.utils.utils as utils
+sys.path.append(os.path.abspath(os.path.join(__file__, "../../../..")))
+
+
+from v2.lib import resource_op
+from v2.lib.admin import UserMgmt
 from v2.lib.exceptions import RGWBaseException, TestExecError
-from v2.lib.resource_op import Config
 from v2.lib.rgw_config_opts import CephConfOp, ConfigOpts
+from v2.lib.s3.auth import Auth
 from v2.lib.s3.write_io_info import BasicIOInfoStructure, BucketIoInfo, IOInfoInitialize
+from v2.lib.s3cmd import auth as s3_auth
+from v2.tests.s3_swift import reusable
+from v2.tests.s3cmd import reusable as s3cmd_reusable
+from v2.utils import utils
 from v2.utils.log import configure_logging
 from v2.utils.test_desc import AddTestInfo
 from v2.utils.utils import RGWService
 
 log = logging.getLogger()
-TEST_DATA_PATH = None
 
 
 def test_exec(config, ssh_con):
+    """
+    Executes test based on configuration passed
+    Args:
+        config(object): Test configuration
+    """
     io_info_initialize = IOInfoInitialize()
     basic_io_structure = BasicIOInfoStructure()
     io_info_initialize.initialize(basic_io_structure.initial())
-    ceph_conf = CephConfOp(ssh_con)
+    write_bucket_io_info = BucketIoInfo()
+    umgmt = UserMgmt()
+    ceph_conf = CephConfOp()
     rgw_service = RGWService()
+
+    ip_and_port = s3cmd_reusable.get_rgw_ip_and_port(ssh_con)
+    if config.haproxy:
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+        port = 5000
+        ip_and_port = f"{ip}:{port}"
+    
+        ceph_conf = CephConfOp(ssh_con)
+    rgw_service = RGWService()
+
     if config.d3n_feature is True:
         log.info("Enabling D3n feature on the cluster")
         data_path_cmd = f"sudo ls {config.datacache_path}"
@@ -126,46 +151,31 @@ def test_exec(config, ssh_con):
         else:
             log.info("RGW service restarted")
 
+        user_info = resource_op.create_users(no_of_users_to_create=config.user_count)
 
-if __name__ == "__main__":
-    test_info = AddTestInfo("Testing D3N-Cache feature enablement")
-    test_info.started_info()
-    try:
-        project_dir = os.path.abspath(os.path.join(__file__, "../../.."))
-        test_data_dir = "test_data"
-        rgw_service = RGWService()
-        TEST_DATA_PATH = os.path.join(project_dir, test_data_dir)
-        log.info("TEST_DATA_PATH: %s" % TEST_DATA_PATH)
-        if not os.path.exists(TEST_DATA_PATH):
-            log.info("test data dir not exists, creating.. ")
-            os.makedirs(TEST_DATA_PATH)
-        parser = argparse.ArgumentParser(description="RGW S3 Automation")
-        parser.add_argument("-c", dest="config", help="RGW Test yaml configuration")
-        parser.add_argument(
-            "-log_level",
-            dest="log_level",
-            help="Set Log Level [DEBUG, INFO, WARNING, ERROR, CRITICAL]",
-            default="info",
-        )
-        parser.add_argument(
-            "--rgw-node", dest="rgw_node", help="RGW Node", default="127.0.0.1"
-        )
-        args = parser.parse_args()
-        yaml_file = args.config
-        rgw_node = args.rgw_node
-        ssh_con = None
-        if rgw_node != "127.0.0.1":
-            ssh_con = utils.connect_remote(rgw_node)
-        log_f_name = os.path.basename(os.path.splitext(yaml_file)[0])
-        configure_logging(f_name=log_f_name, set_level=args.log_level.upper())
-        config = Config(yaml_file)
-        ceph_conf = CephConfOp(ssh_con)
-        config.read(ssh_con)
-        test_exec(config, ssh_con)
-        test_info.success_status("test passed")
-        sys.exit(0)
-    except (RGWBaseException, Exception) as e:
-        log.error(e)
-        log.error(traceback.format_exc())
-        test_info.failed_status("test failed")
-        sys.exit(1)
+        user_info = resource_op.create_users(no_of_users_to_create=config.user_count)
+        s3_auth.do_auth(user_info[0], ip_and_port)
+        auth = Auth(user_info[0], ssh_con, ssl=config.ssl, haproxy=config.haproxy)
+        rgw_conn = auth.do_auth()
+        for bc in range(config.bucket_count):
+            bucket_name = utils.gen_bucket_name_from_userid(
+                user_info[0]["user_id"], rand_no=bc
+            )
+            s3cmd_reusable.create_bucket(bucket_name)
+            log.info(f"Bucket {bucket_name} created")
+            s3cmd_path = "/home/cephuser/venv/bin/s3cmd"
+
+            log.info(f"uploading objects greater than 4MB to bucket {bucket_name}")
+            utils.exec_shell_cmd(f"fallocate -l 10M obj10m")
+            cmd = f"{s3cmd_path} put obj10m s3://{bucket_name}/object1"
+            utils.exec_shell_cmd(cmd)
+
+            cmd = f"{s3cmd_path} get s3://{bucket_name}/object1 /dev/shm/object1_get.dat"
+            utils.exec_shell_cmd(cmd)
+
+            log.info(f"Check cache created")
+            cmd = f"ls -lh {datacache_path}"
+            out = utils.exec_shell_cmd(cmd)
+
+            cmd = f"{s3cmd_path} get s3://{bucket_name}/object1 /dev/shm/object1_get2.dat"
+            utils.exec_shell_cmd(cmd)
